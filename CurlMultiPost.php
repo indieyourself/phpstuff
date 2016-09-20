2c1f79a36b78817da05de87565bd817b82317894
HanWenfang <eehanwenfang@gmail.com>
2016-09-20 18:01:00 +0800

    multi curl

--- CurlMultiPost.php
+++ CurlMultiPost.php
@@ -28,20 +28,20 @@ function multipost( $url, $data, $num) {
 		curl_multi_select($mch);
 	} while($active > 0 );
 	
-	foreach ( $chs as$ch ) {
+	foreach ( $chs as $ch ) {
 		$response = curl_multi_getcontent($ch);
 		$errno    = curl_errno($ch);
 		if ($errno != 0) {
 			echo( "post errno=$errno response=$response");
 		}
 		
-		curl_multi_remove_handle($chs, $ch);
+		curl_multi_remove_handle($mch, $ch);
 	}
 	
 	curl_multi_close($mch);
 }
 
-$url = "http://192.168.10.92:8083/bc/log?_t=123456&_v=1.0&_os=1&channel=99999";
+$url = "http://192.168.10.92:8083/bc/log?_t=123456&_v=1.0&_os=1&_channel=99999";
 $data = array(
 
 );
